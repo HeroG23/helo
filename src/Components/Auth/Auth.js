125183e9a3c68b2f88af057da390b438f2b9a419
Brian Demro <demrobrian@hotmail.com>
2020-11-23 12:15:00 -0600

    added styling to Auth and Form

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
+import './Auth.css'
 
 import { updateUser } from "../../redux/reducer";
 
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
+import './Form.css'
 
 class Form extends Component {
   constructor(props) {
