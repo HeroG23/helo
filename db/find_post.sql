c84db7a39421ff439e23b6798de9d31616dd5a33
Brian Demro <demrobrian@hotmail.com>
2020-12-02 18:13:43 -0600

    stuff?

--- db/find_post.sql
+++ db/find_post.sql
@@ -1,4 +1,4 @@
 SELECT p.post_id, p.title, p.img, p.author_id, u.username, u.profile_pic 
-from posts p
-join users u ON u.user_id = p.author_id
+FROM posts p
+JOIN users u ON u.user_id = p.author_id
 WHERE p.post_id = $1;
